aef89a55cd573265a06a77b2e69d41b5322e92e6
wattnotions <shaneormonde@gmail.com>
2023-11-06 18:42:35 +0000

    rotated cam 90 degrees ccw

--- website/keyandvid/app.py
+++ website/keyandvid/app.py
@@ -75,6 +75,7 @@ def index():
 def gen():
     """Video streaming generator function."""
     with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
+        camera.rotation = 270  # Rotate the camera stream by 90 degrees counter-clockwise
         output = StreamingOutput()
         camera.start_recording(output, format='mjpeg')
         try:
